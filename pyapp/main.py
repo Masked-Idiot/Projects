66d890271da5646bdd589085ac16ee9960561a61
Liam Brinkerhoff (BLAZE02) <101909445+Masked-Idiot@users.noreply.github.com>
2023-11-10 22:39:47 +0000

    AAAA

--- pyapp/main.py
+++ pyapp/main.py
@@ -3,3 +3,5 @@ from pyscript import document
 def cool_button(event):
     output = document.querySelector("#output")
     thingy = document.createElement("p")
+    thingy.innerHTML = "this is a certified cool thingy"
+    output.appendChild(thingy)
