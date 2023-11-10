4c8235da4aef48d039629c7ed4d24931329ffb79
Liam Brinkerhoff (BLAZE02) <101909445+Masked-Idiot@users.noreply.github.com>
2023-11-10 23:20:45 +0000

    HELP

--- pyapp/main.py
+++
@@ -1,5 +0,0 @@
-def cool_button(event):
-    output = document.querySelector("#output")
-    thingy = document.createElement("p")
-    thingy.innerHTML = "this is a certified cool thingy"
-    output.appendChild(thingy)
